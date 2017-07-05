9d3facb355ced04a18b7033ae87101749ab66b9b
庞小飞 <1848651385@qq.com>
2017-07-05 17:12:29 +0800

    Create publish.php

---
+++ subscribe-publish/publish.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * redis实战
+ *
+ * 发布
+ * @example php publish.php
+ */
+  //发布
+  $redis = new \Redis();
+  $redis->connect('127.0.0.1', 6379);
+  $redis->publish('msg', '来自msg频道的推送');
+  echo "msg频道消息推送成功～ \n";
+  $redis->close();
